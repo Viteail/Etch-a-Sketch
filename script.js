fe86b31551138acd6a60ae8133b90436e8514faa
Viteail <vitea.sirbu1@gmail.com>
2022-04-22 17:59:51 +0300

    Edit script.js, index.html, styles.css

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-const getArea = document.querySelector(".area");
+const getContainer = document.querySelector(".container");
 const getOptions = document.querySelector(".options");
 const getColorSelection = document.querySelector("#color-select");
+const getSquareSelection = document.querySelector("#vol-select");
+
+const Area = document.createElement("div");
+Area.classList.add("area");
+getContainer.appendChild(Area);
 
 const BUTTONCLEARCOLOR = document.createElement("button");
 BUTTONCLEARCOLOR.textContent = "Clear";
 getOptions.appendChild(BUTTONCLEARCOLOR);
+BUTTONCLEARCOLOR.addEventListener("click", squareSelection);
 
-for (let i = 0; i < 16; i++) {
-  const box = document.createElement("div");
-  getArea.appendChild(box);
+function squareSelection() {
+  Area.textContent = "";
+  const vol = getSquareSelection.value;
+  Area.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("box"))
+      e.target.style.backgroundColor = getColorSelection.value;
+  });
 
-  box.addEventListener(
-    "mouseover",
-    () => (box.style.backgroundColor = getColorSelection.value)
-  );
-  BUTTONCLEARCOLOR.addEventListener(
-    "click",
-    () => (box.style.backgroundColor = "white")
-  );
+  for (let i = 0; i < vol * vol; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    Area.appendChild(box);
+
+    Area.style.gridTemplateColumns = `repeat(${vol}, calc(500px / ${vol}))`;
+    Area.style.gridAutoRows = `calc(500px / ${vol}))`;
+  }
 }
+
+getSquareSelection.addEventListener("input", showSquareSelect);
+getSquareSelection.addEventListener("change", squareSelection);
+
+function showSquareSelect() {
+  const SHOWSQUARESELECT = document.querySelector(".show-vol-select");
+  SHOWSQUARESELECT.textContent =
+    `${getSquareSelection.value}` + " x " + `${getSquareSelection.value}`;
+}
+
+squareSelection();
